8a88c18b27bbd4173d014371e56c0100a56284e2
Ramone Robertson <ramonerobertson10@gmail.com>
2025-03-13 08:57:05 +0900

    Spring New

---
+++ src/main/java/app/trekz/TrekzApplication.java
@@ -0,0 +1,13 @@
+package app.trekz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekzApplication.class, args);
+	}
+
+}
