56279f680482266875dcd92e949782bf395d0173
Ramone Robertson <ramonerobertson10@gmail.com>
2025-03-16 15:25:50 +0900

    Added packages for run class

---
+++ src/main/java/entities/Run.java
@@ -0,0 +1,9 @@
+package entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
+
+public class Run {
+}
