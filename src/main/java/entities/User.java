d12548051cea1a19ad1684f4f4f27c2770800743
Ramone Robertson <ramonerobertson10@gmail.com>
2025-03-16 17:57:43 +0900

    Added user enttity properties with some validations

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,12 +1,57 @@
 package entities;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
 import java.util.Date;
 
 @Table(name = "users")
 @Entity
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(unique = true, length = 100, nullable = false)
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "Invalid email"
+    )
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    private String password;
+
+    @Column(length = 100)
+    @Size(min = 1, max = 100, message = "First name must be between 1 and 100 characters")
+    private String name;
+
+    @Column(nullable = false)
+    @NotNull(message = "Birthdate is required")
+    @Past(message = "Birthdate must be in the past")
+    private LocalDate birthday;
+
+    @CreationTimestamp
+    @Column(nullable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false, name= "updated_at")
+    private Date updatedAt;
+
+    //  Getters and Setters
+
 
 }
