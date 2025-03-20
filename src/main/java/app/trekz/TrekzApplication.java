6c4bf0173b03a849a026eb2202803ae7a12d2203
Ramone Robertson <ramonerobertson10@gmail.com>
2025-03-20 17:51:14 +0900

    Finished JWT service and added env varibles for DB and JWT properties

--- src/main/java/app/trekz/TrekzApplication.java
+++ src/main/java/app/trekz/TrekzApplication.java
@@ -1,5 +1,6 @@
 package app.trekz;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrekzApplication {
 
 	public static void main(String[] args) {
+		// Load env variables
+		Dotenv dotenv = Dotenv.load();
+
+		// Set system properties
+
+		// DB
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+
+		// JWT Settings
+		System.setProperty("security.jwt.secret-key", dotenv.get("JWT_SECRET"));
+		System.setProperty("security.jwt.expiration-time", dotenv.get("JWT_EXPIRATION"));
+
 		SpringApplication.run(TrekzApplication.class, args);
 	}
 
---
+++ src/main/java/app/trekz/config/ApplicationConfig.java
@@ -0,0 +1,4 @@
+package app.trekz.config;
+
+public class ApplicationConfig {
+}
